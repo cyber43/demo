f8ad779e89195688174a86b4b1c81c628b3095d2
star <cyber43@users.noreply.github.com>
2022-04-04 17:06:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "good job. good job \n"
+    return "good job. good job docker test \n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
